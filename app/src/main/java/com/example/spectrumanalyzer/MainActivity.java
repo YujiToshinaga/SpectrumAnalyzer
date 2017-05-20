1aa3f83ca0c4e74bb819759909331d1eec2dfcea
Yuji Toshinaga <y.toshinaga@gmail.com>
2017-05-21 00:11:41 +0900

    最初のコミット

---
+++ app/src/main/java/com/example/spectrumanalyzer/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.spectrumanalyzer;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final int RECORD_AUDIO_PERMISSION = 0x1;
+    private ToggleButton mToggleButtonPlay;
+    private EditText mEditTextFreq1;
+    private AudioManager mAudioManager;
+    private WavePlayer mWavePlayer;
+    private int mSaveVolume;
+    private int mVolume;
+    private int mFreq1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        permitAudioRecord();
+
+        // 描画用変数を初期化
+        mToggleButtonPlay = (ToggleButton)findViewById(R.id.toggleButtonPlay);
+        mEditTextFreq1 = (EditText)findViewById(R.id.editTextFreq1);
+
+        mAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        mWavePlayer = new WavePlayer(0);
+        mSaveVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mVolume = mSaveVolume;
+        mFreq1 = 0;
+
+        // -------- Play Music --------
+        mToggleButtonPlay.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.d(TAG, "Play");
+                    mFreq1 = Integer.parseInt(mEditTextFreq1.getText().toString());
+                    mWavePlayer.changeParam(mFreq1);
+                    mWavePlayer.play();
+                } else {
+                    Log.d(TAG, "Stop");
+                    mWavePlayer.stop();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        mSaveVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mVolume, 0);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mSaveVolume, 0);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mSaveVolume, 0);
+    }
+
+    // RECORD_AUDIOの許可
+    private void permitAudioRecord() {
+        // 以前に許可されたかどうかチェックする
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            // 許可を求める
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_AUDIO_PERMISSION);
+            }
+        } else {
+            // 以前に許可されているので何もしない
+        }
+    }
+
+    // RECORD_AUDIOの許可結果を受け取る
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RECORD_AUDIO_PERMISSION:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Toast.makeText(MainActivity.this, "AUDIO_RECORD OK!",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "AUDIO_RECORD NG!",Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/spectrumanalyzer/WavePlayer.java
@@ -0,0 +1,106 @@
+package com.example.spectrumanalyzer;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.util.Log;
+
+/**
+ * Created by usr1 on 2017/05/20.
+ */
+
+public class WavePlayer {
+    private static final String TAG = "WavePlayer";
+    private static final int SAMPLERATE= 44100;
+    private AudioTrack mAudioTrack;
+    private WaveGen mWaveGen;
+    private int mFreq;
+    private int mSamples;
+
+    WavePlayer(int freq) {
+        int bufferSize;
+
+        Log.d(TAG, "Initialize WavePlayer");
+
+        mFreq = freq;
+
+        // 必要となるバッファサイズを計算する
+        bufferSize = AudioTrack.getMinBufferSize(
+                SAMPLERATE,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT
+        );
+
+        // AudioTrackを初期化する
+        mAudioTrack = new AudioTrack(
+                AudioManager.STREAM_MUSIC,
+                SAMPLERATE,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                bufferSize,
+                AudioTrack.MODE_STREAM
+        );
+
+        // 一度に書き込むサンプル数を計算する
+        mSamples = bufferSize / 4;
+
+        // WaveGenを初期化する
+        mWaveGen = new WaveGen(mFreq, SAMPLERATE, mSamples);
+
+        // AudioTrack再生の設定をする
+        mAudioTrack.setPlaybackPositionUpdateListener(
+                new AudioTrack.OnPlaybackPositionUpdateListener() {
+                    public void onPeriodicNotification(AudioTrack track) {
+                        short[] buf;
+                        buf = mWaveGen.getBuf();
+                        mAudioTrack.write(buf, 0, buf.length);
+                    }
+
+                    public void onMarkerReached(AudioTrack track) {
+                    }
+                }
+        );
+
+        // Listnerを呼び出す周期を設定する
+        mAudioTrack.setPositionNotificationPeriod(mSamples);
+    }
+
+    public void changeParam(int freq) {
+        // エラー処理
+        if (mAudioTrack == null) {
+            Log.d(TAG, "start : not initialized");
+            return;
+        }
+
+        // パラメータを変更する
+        mWaveGen.changeParam(freq);
+    }
+
+    public void play() {
+        // エラー処理
+        if (mAudioTrack == null) {
+            Log.d(TAG, "start : not initialized");
+            return;
+        }
+
+        // 再生する
+        short[] buf;
+        buf = mWaveGen.getBuf();
+        mAudioTrack.write(buf, 0, buf.length);
+        buf = mWaveGen.getBuf();
+        mAudioTrack.write(buf, 0, buf.length);
+        mAudioTrack.play();
+
+    }
+
+    public void stop() {
+        // エラー処理
+        if (mAudioTrack == null) {
+            Log.d(TAG, "start : not initialized");
+            return;
+        }
+
+        // 停止する
+        mAudioTrack.stop();
+    }
+}
